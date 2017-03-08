f73f560a85e34724857db6a218de3c7ad6d3f12f
Lakmal Caldera <lakmal.developer@gmail.com>
2017-03-08 23:41:36 +0530

    added static libraries to be hosted from lib

--- app/datatables/transactions_datatable.rb
+++ app/datatables/transactions_datatable.rb
@@ -4,9 +4,9 @@ class TransactionsDatatable < AjaxDatatablesRails::Base
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
     @view_columns ||={
-        uid: {source: "Transaction.uid", cond: filter_custom_column_condition},
+        agent: {source: "Transaction.agent", cond: filter_custom_column_condition},
         customer: {source: "Transaction.customer", cond: filter_custom_column_condition},
-        timestamp: {source: "Transaction.timestamp", cond: filter_date_column_condition},
+        timestamp: {source: "Transaction.timestamp", cond: filter_date_range_condition},
         status: {source: "Transaction.status", cond: filter_custom_column_condition},
         mobile: {source: "Transaction.mobile", cond: filter_custom_column_condition},
         amount: {source: "Transaction.amount", cond: filter_custom_column_condition}
@@ -17,13 +17,12 @@ class TransactionsDatatable < AjaxDatatablesRails::Base
     records.map do |record|
       {
           # example:
-          uid: record.uid,
+          agent: record.agent,
           customer: record.customer,
           timestamp: record.timestamp,
           status: record.status,
           mobile: record.mobile,
-          amount: record.amount,
-          totalAmount: records.count
+          amount: record.amount
       }
     end
   end
@@ -33,7 +32,7 @@ class TransactionsDatatable < AjaxDatatablesRails::Base
         :draw => params[:draw].to_i,
         :TotalAmount => get_raw_records.sum(:amount),
         :filteredTotalAmount => filter_records(get_raw_records).sum(:amount),
-        :recordsTotal =>  get_raw_records.count(:all),
+        :recordsTotal => get_raw_records.count(:all),
         :recordsFiltered => filter_records(get_raw_records).count(:all),
         :data => data
     }
@@ -49,9 +48,16 @@ class TransactionsDatatable < AjaxDatatablesRails::Base
   end
 
   def filter_date_column_condition
-    ->(column) { Transaction.arel_table[:timestamp].gteq(column.search.value.to_i).and(Transaction.arel_table[:timestamp].lt(column.search.value.to_i + 86400))}
+    ->(column) { Transaction.arel_table[:timestamp].gteq(column.search.value.to_i).and(Transaction.arel_table[:timestamp].lt(column.search.value.to_i + 86400)) }
   end
 
+  def filter_date_range_condition
+    ->(column) {
+      unix_timestamp_min = column.search.value.split("-")[0].to_i
+      unix_timestamp_max = column.search.value.split("-")[1].to_i + 86400
+      Transaction.arel_table[:timestamp].gteq(unix_timestamp_min).and(Transaction.arel_table[:timestamp].lt(unix_timestamp_max))
+    }
+  end
 end
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'ip', port: 3000 }
 
+
+
 end
